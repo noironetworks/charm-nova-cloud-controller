a0208798da2dd457324b7dfed11b7df46443f4ec
Adam Gandelman <adamg@canonical.com>
2013-08-19 17:38:53 -0700

    Only run identity_joined() from cluster leader.

--- hooks/nova_cc_hooks.py
+++ hooks/nova_cc_hooks.py
@@ -150,6 +150,8 @@ def image_service_changed():
 
 @hooks.hook('identity-service-relation-joined')
 def identity_joined(rid=None):
+    if eligible_leader(CLUSTER_RES):
+        return
     base_url = canonical_url(CONFIGS)
     relation_set(relation_id=rid, **determine_endpoints(base_url))
 
