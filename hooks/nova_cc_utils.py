db163fb3582a51d40632bd8a9d35cfbd4b0fb76d
Louis Bouchard <louis.bouchard@canonical.com>
2014-05-02 18:07:52 +0200

    Functional line by line modification, except for the 'nova' user.
    Need more work on the 'nova' specific case

--- hooks/nova_cc_utils.py
+++ hooks/nova_cc_utils.py
@@ -603,21 +603,23 @@ def ssh_compute_add(public_key, user=None):
         add_authorized_key(public_key, user)
 
 
-def ssh_known_hosts_lines_b64(user=None):
+def ssh_known_hosts_lines(user=None):
     known_hosts_list = []
 
     with open(known_hosts(user)) as hosts:
         for hosts_line in hosts:
-            known_hosts_list.append(b64encode(hosts_line.rstrip()))
+            if hosts_line.rstrip():
+                known_hosts_list.append(hosts_line.rstrip())
     return(known_hosts_list)
 
 
-def ssh_authorized_keys_lines_b64(user=None):
+def ssh_authorized_keys_lines(user=None):
     authorized_keys_list = []
 
-    with open(authorized_keys(user)) as hosts:
-        for authkey_line in hosts:
-            authorized_keys_list.append(b64encode(authkey_line.rstrip()))
+    with open(authorized_keys(user)) as keys:
+        for authkey_line in keys:
+            if authkey_line.rstrip():
+                authorized_keys_list.append(authkey_line.rstrip())
     return(authorized_keys_list)
 
 
