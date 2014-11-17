fcf6b86f54bff138b7a1a5d48a29a543c141b141
Brad Marshall <brad.marshall@canonical.com>
2014-11-17 13:43:24 +1000

    [bradm] Add monitoring for sysvinit daemons, use services() instead of hard coded daemon list, pep8 fixes

--- hooks/nova_cc_hooks.py
+++ hooks/nova_cc_hooks.py
@@ -856,14 +856,6 @@ def neutron_api_relation_broken():
 
 @hooks.hook('nrpe-external-master-relation-joined', 'nrpe-external-master-relation-changed')
 def update_nrpe_config():
-    SERVICES = [
-        'nova-api-os-compute',
-        'nova-api-ec2',
-        'nova-cert',
-        'nova-objectstore',
-        'nova-scheduler',
-        'nova-conductor',
-    ]
     # Find out if nrpe set nagios_hostname
     hostname = None
     host_context = None
@@ -880,12 +872,29 @@ def update_nrpe_config():
     else:
         current_unit = local_unit()
 
-    for service in SERVICES:
-        nrpe.add_check(
-            shortname=service,
-            description='process check {%s}' % current_unit,
-            check_cmd = 'check_upstart_job %s' % service,
-            )
+    services_to_monitor = services()
+    for service in services_to_monitor:
+        upstart_init = '/etc/init/%s.conf' % service
+        sysv_init = '/etc/init.d/%s' % service
+
+        if os.path.exists(upstart_init):
+            nrpe.add_check(
+                shortname=service,
+                description='process check {%s}' % current_unit,
+                check_cmd = 'check_upstart_job %s' % service,
+                )
+        elif os.path.exists(sysv_init):
+            cronpath = '/etc/cron.d/nagios-service-check-%s' % service
+            checkpath = os.path.join(os.environ['CHARM_DIR'], 'files/nrpe-external-master', 'check_exit_status.pl'),
+            cron_template = '*/5 * * * * root %s -s /etc/init.d/%s status > /var/lib/nagios/service-check-%s.txt\n' % (checkpath[0], service, service)
+            f = open(cronpath, 'w')
+            f.write(cron_template)
+            f.close()
+            nrpe.add_check(
+                shortname=service,
+                description='process check {%s}' % current_unit,
+                check_cmd = 'check_status_file.py -f /var/lib/nagios/service-check-%s.txt' % service,
+                )
 
     nrpe.write()
 
