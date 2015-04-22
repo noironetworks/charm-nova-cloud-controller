8a748906a26c579f456c7f26f56ee9f13bed9dfb
Liam Young <liam.young@canonical.com>
2015-04-22 13:49:57 +0000

    Stop running db migrations when deployment is kilo or migrating to kilo

--- hooks/nova_cc_hooks.py
+++ hooks/nova_cc_hooks.py
@@ -209,6 +209,9 @@ def conditional_neutron_migration():
     if os_release('nova-common') <= 'icehouse':
         log('Not running neutron database migration as migrations are handled'
             'by the neutron-server process.')
+    elif os_release('nova-common') >= 'kilo':
+        log('Not running neutron database migration as migrations are by'
+            'by neutron-api charm.')
     else:
         migrate_neutron_database()
         # neutron-api service may have appeared while the migration was
