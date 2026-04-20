004a9b77a67e9989f139bdfe470645e711253f98
Felipe Reyes <felipe.reyes@canonical.com>
2026-04-20 11:05:57 -0400

    Ubuntu 24.04 (Noble) support fixes
    Summary of changes:
    - Add py312 to tox.ini, the requirements used are based on the ones
      from py310 to keep them the closest to Jammy and prevent they diverge
      too much.
    - Bump up setuptools to 68.2.2 in src/build.lock to make it compatible
      with Python 3.12
    - Pass --use-lock-file-branches in reactive charms
    - Use charm-tools from 3.x/stable channel now that it has been released
    - Use charmcraft 3.x for building (updated .zuul.yaml and osci.yaml)
    - Updated flake8 to 7.1.1 instead of 3.9.2
    - Use charms.reactive from https://github.com/canonical/charms.reactive,
      because the version availably in pypi is incompatible with py312
    - pep8 fixes
    - unit tests fixes
    
    Change-Id: Ib3a927321944bc66cf50d9e3e5e3867d220ea423
    Signed-off-by: Felipe Reyes <felipe.reyes@canonical.com>

--- unit_tests/test_nova_cc_utils.py
+++ unit_tests/test_nova_cc_utils.py
@@ -67,7 +67,7 @@ TO_PATCH = [
 ]
 
 SCRIPTRC_ENV_VARS = {
-    'OPENSTACK_PORT_MASTPORT': 5404,
+    'OPENSTACK_PORT_MCASTPORT': 5404,
     'OPENSTACK_SERVICE_API_EC2': 'nova-api-ec2',
     'OPENSTACK_SERVICE_API_OS_COMPUTE': 'nova-api-os-compute',
     'OPENSTACK_SERVICE_CERT': 'nova-cert',
@@ -648,7 +648,7 @@ class NovaCCUtilsTests(CharmTestCase):
     def test_save_script_rc_base(self):
         self.relation_ids.return_value = []
         utils.save_script_rc()
-        self.save_script_rc.called_with(**SCRIPTRC_ENV_VARS)
+        self.save_script_rc.assert_called_with(**SCRIPTRC_ENV_VARS)
 
     @patch('charmhelpers.contrib.openstack.utils.lsb_release')
     def test_get_step_upgrade_source_target_liberty(self, lsb_release):
